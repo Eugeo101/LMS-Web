3e1f5d9308d6ad972b14bc3e3e37eb92369210e7
iMegz <ammhaa2012@gmail.com>
2022-05-17 22:13:45 +0200

    Added frontend

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { connect, connection: conn } = require("mongoose");
 const errorsMW = require("./middlewares/errors");
-
+const comp = require("compression");
+const helmet = require("helmet");
 const routes = require("./routers/index.js");
 
 const inProduction = process.env.NODE_ENV == "production";
@@ -10,8 +11,14 @@ if (!inProduction) require("dotenv").config();
 connect("mongodb+srv://lms:lms@cluster0.srk19.mongodb.net/lms?retryWrites=true&w=majority", {});
 
 const app = express();
-app.use(express.json());
+
 app.use(require("cors")());
+app.use(comp());
+app.use(helmet.xssFilter());
+app.use(express.json());
+app.use(express.static("public"));
+
+
 app.use("/api", routes);
 app.use(errorsMW);
 
